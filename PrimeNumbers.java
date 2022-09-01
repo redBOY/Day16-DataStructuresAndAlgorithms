ff6e5b223698939b5a436312f9178d0de964de93
redBOY <43438363+redBOY@users.noreply.github.com>
2022-09-01 17:29:29 +0530

    PrimeNumbers.java

---
+++ PrimeNumbers.java
@@ -0,0 +1,29 @@
+package com.bridgelabzDatastructureandalgorithm;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		System.out.print("Prime Number between 0-1000 are : ");
+		for (int num = 0; num <= 1000; num++) {
+			if (isPrime(num)) {
+				System.out.print(num + " ");
+			}
+			System.out.print("");			
+		}
+	}
+	
+	public static boolean isPrime(int num) {
+		if (num == 0 || num == 1) {
+			return false;
+		}
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	}
+
